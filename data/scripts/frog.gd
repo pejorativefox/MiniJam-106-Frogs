3315b9ba7437a9b09669de1b4b885df353238675
Chris Statzer <chris.statzer@gmail.com>
2022-05-13 20:48:30 +0800

    Motion working, prototype of gameplay done.

--- data/scripts/frog.gd
+++ data/scripts/frog.gd
@@ -1,33 +1,54 @@
 extends KinematicBody2D
 
-const GRAVITY = 200.0
+const GRAVITY = 400.0
 const WALK_SPEED = 50.0
+const ACCEL = 1028
+const MAX_SPEED = 256
+const FRICTION = 1
+const AIR_RESIST = 0.02
+const JUMP_FORCE = 400
 
 var velocity = Vector2()
 
-onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+onready var player_sprite = $Sprite
 
 func _ready():
 	pass # Replace with function body.
 
+
 func _unhandled_input(event):
-	if Input.is_action_pressed("jump"):
-		print_debug("jump!");
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -WALK_SPEED
-	if Input.is_action_pressed("move_right"):
-		velocity.x = WALK_SPEED
+	pass
 
 
 func _physics_process(delta):
+	var x_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if x_input != 0:
+		velocity.x += x_input * ACCEL * delta
+		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+		player_sprite.flip_h = x_input < 0
+		
+	else:
+		pass
+		
+	velocity.y += GRAVITY * delta
 	
-	velocity.y += gravity * delta
-
-	#var motion = velocity * delta
-	print_debug(velocity)
-	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
-	#move_and_collide(motion)
-	#move_and_slide(motion, Vector2(0, -1))
+	if is_on_floor():
+		if x_input == 0:
+			velocity.x = lerp(velocity.x, 0, FRICTION)
+			player_sprite.play("idle")
+		else:
+			player_sprite.play("walk")
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -JUMP_FORCE
+	else:
+		player_sprite.play("jump")
+		if Input.is_action_just_released("jump") and velocity.y < -JUMP_FORCE/2:
+			velocity.y = -JUMP_FORCE/2
+			
+		if x_input == 0:
+			velocity.x = lerp(velocity.x, 0, AIR_RESIST)
+			
+	velocity = move_and_slide(velocity, Vector2(0, -1))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

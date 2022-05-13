0578c3977e7fd342939035f46aaffae7be3cf104
Chris Statzer <chris.statzer@gmail.com>
2022-05-13 19:29:43 +0800

    Added platforms, working on physics

--- data/scripts/frog.gd
+++ data/scripts/frog.gd
@@ -1,15 +1,33 @@
-extends Node
+extends KinematicBody2D
 
+const GRAVITY = 200.0
+const WALK_SPEED = 50.0
 
-var movementVector = Vector2(0, 0);
+var velocity = Vector2()
+
+onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	pass # Replace with function body.
 
 func _unhandled_input(event):
 	if Input.is_action_pressed("jump"):
-		movementVector += Vector2(0, -1);
 		print_debug("jump!");
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -WALK_SPEED
+	if Input.is_action_pressed("move_right"):
+		velocity.x = WALK_SPEED
+
+
+func _physics_process(delta):
+	
+	velocity.y += gravity * delta
+
+	#var motion = velocity * delta
+	print_debug(velocity)
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	#move_and_collide(motion)
+	#move_and_slide(motion, Vector2(0, -1))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
